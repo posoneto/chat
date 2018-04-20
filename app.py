928ee6aa4e5276d9629e5a1dd106b329eef48c67
posoneto <walter_figueroa@hotmail.com>
2018-04-20 17:28:35 -0500

    nose

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def main():
     dialogue_manager.create_chitchat_bot()
     print("Ready to talk!")
 
-    host = '0.0.0.0'
+    host = '127.0.0.1'
     port = 3979
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
