62add01edcb9cf3275ab02d129292f5e80db1e40
posoneto <walter_figueroa@hotmail.com>
2018-04-20 03:50:13 -0500

    scipy

--- dialogue_manager.py
+++ dialogue_manager.py
@@ -5,6 +5,7 @@ import pip
 pip.main(['install', 'sklearn'])
 pip.main(['install', 'chatterbot'])
 pip.main(['install', 'numpy'])
+pip.main(['install', 'scipy'])
 
 from sklearn.metrics.pairwise import pairwise_distances_argmin
 
